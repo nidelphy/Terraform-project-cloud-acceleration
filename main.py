0ae5e010182aba564c2e56f70eff2ec2210456ee
michaelO <michael_oladugba@bmc.com>
2023-10-30 03:44:19 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import boto3
+
+# Create an S3 client
+s3 = boto3.client('s3', region_name='us-east-2')
+# Specify the name of your S3 bucket
+bucket_name = 'your-bucket-name'
+
+# List objects in the bucket
+response = s3.list_objects_v2(Bucket=bucket_name)
+
+# Print the object names (Object Key)
+if 'Contents' in response:
+    for obj in response['Contents']:
+        print(f"Object Key: {obj['Key']}")
+else:
+    print("The bucket is empty.")
